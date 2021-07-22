a327585ba9ff4e0f7ad947fccc36162cd23f5b29
GZW <9414676+gzwrrr@user.noreply.gitee.com>
2021-07-22 14:05:47 +0800

    管理系统功能完成

--- src/main/Java/com/gzw/servlet/provider/ProviderServlet.java
+++ src/main/Java/com/gzw/servlet/provider/ProviderServlet.java
@@ -2,7 +2,10 @@ package com.gzw.servlet.provider;
 
 import com.alibaba.fastjson.JSONArray;
 import com.gzw.pojo.Provider;
+import com.gzw.pojo.User;
+import com.gzw.service.provider.ProviderService;
 import com.gzw.service.provider.ProviderServiceImpl;
+import com.gzw.util.Constants;
 import com.mysql.cj.util.StringUtils;
 
 import javax.servlet.ServletException;
@@ -11,6 +14,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 
@@ -18,13 +22,85 @@ public class ProviderServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         String method = req.getParameter("method");
-        if ("query".equals(method))
+        if(method != null && method.equals("query")){
             this.query(req,resp);
-        else if("add".equals(method))
+        }else if(method != null && method.equals("add")){
             this.add(req,resp);
-        else if("delprovider".equals(method))
-            this.delete(req,resp);
+        }else if(method != null && method.equals("view")){
+            this.getProviderById(req,resp,"providerview.jsp");
+        }else if(method != null && method.equals("modify")){
+            this.getProviderById(req,resp,"providermodify.jsp");
+        }else if(method != null && method.equals("modifysave")){
+            this.modify(req,resp);
+        }else if(method != null && method.equals("delprovider")){
+            this.delProvider(req,resp);
+        }
 
+    }
+
+
+    private void delProvider(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        String id = request.getParameter("proid");
+        HashMap<String, String> resultMap = new HashMap<String, String>();
+        if(!StringUtils.isNullOrEmpty(id)){
+            ProviderService providerService = new ProviderServiceImpl();
+            int flag = providerService.deleteProviderById(id);
+            if(flag == 0){//删除成功
+                resultMap.put("delResult", "true");
+            }else if(flag == -1){//删除失败
+                resultMap.put("delResult", "false");
+            }else if(flag > 0){//该供应商下有订单，不能删除，返回订单数
+                resultMap.put("delResult", String.valueOf(flag));
+            }
+        }else{
+            resultMap.put("delResult", "notexit");
+        }
+        //把resultMap转换成json对象输出
+        response.setContentType("application/json");
+        PrintWriter outPrintWriter = response.getWriter();
+        outPrintWriter.write(JSONArray.toJSONString(resultMap));
+        outPrintWriter.flush();
+        outPrintWriter.close();
+    }
+
+    private void modify(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        String proContact = request.getParameter("proContact");
+        String proPhone = request.getParameter("proPhone");
+        String proAddress = request.getParameter("proAddress");
+        String proFax = request.getParameter("proFax");
+        String proDesc = request.getParameter("proDesc");
+        String id = request.getParameter("id");
+        Provider provider = new Provider();
+        provider.setId(Integer.valueOf(id));
+        provider.setProContact(proContact);
+        provider.setProPhone(proPhone);
+        provider.setProFax(proFax);
+        provider.setProAddress(proAddress);
+        provider.setProDesc(proDesc);
+        provider.setModifyBy(((User)request.getSession().getAttribute(Constants.USER_SESSION)).getId());
+        provider.setModifyDate(new Date());
+        boolean flag = false;
+        ProviderService providerService = new ProviderServiceImpl();
+        flag = providerService.modify(provider);
+        if(flag){
+            response.sendRedirect(request.getContextPath()+"/jsp/provider.do?method=query");
+        }else{
+            request.getRequestDispatcher("providermodify.jsp").forward(request, response);
+        }
+    }
+
+    private void getProviderById(HttpServletRequest request, HttpServletResponse response,String url)
+            throws ServletException, IOException {
+        String id = request.getParameter("proid");
+        if(!StringUtils.isNullOrEmpty(id)){
+            ProviderService providerService = new ProviderServiceImpl();
+            Provider provider = null;
+            provider = providerService.getProviderById(id);
+            request.setAttribute("provider", provider);
+            request.getRequestDispatcher(url).forward(request, response);
+        }
     }
 
     private void add(HttpServletRequest req, HttpServletResponse resp) throws  ServletException ,IOException{
--- src/main/webapp/jsp/providermodify.jsp
+++ src/main/webapp/jsp/providermodify.jsp
@@ -9,7 +9,8 @@
       </div>
       <div class="providerAdd" id="providerAdd-box">
           <form id="providerForm" name="providerForm" method="post" action="${pageContext.request.contextPath }/jsp/provider.do">
-              <!--div的class 为error是验证错误，ok是验证成功-->
+              <input type="hidden" name="method" value="modifysave">
+              <input type="hidden" name="id" value="${provider.id }">
               <div class="provider-common" id="pList-1">
                   <label for="proCode">供应商编码：</label>
                   <input type="text" name="proCode" id="proCode" value="${provider.proCode }" readonly="readonly"> 
